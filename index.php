eb7b4ec5dd8d744fc487398928a60ddefce1d454
Jo Cichon <cichon.jo@student.greenriver.edupwdgit config --global user.email cichon.jo@student.greenriver.edu>
2023-10-22 14:22:23 -0400

    added views directory with home.html file

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+//instance of Base class for fatfree
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+}
+);
+
+//run fatfree
+$f3->run();
+
+
